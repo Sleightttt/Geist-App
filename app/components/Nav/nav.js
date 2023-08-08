8212b6968584ca7e88233e9eca88317c48e43fdb
Sleightttt <brennancota@yahoo.com>
2023-08-08 03:08:52 -0700

    adding nav icons

--- app/components/Nav/nav.js
+++ app/components/Nav/nav.js
@@ -1,19 +1,23 @@
 // Nav.js
 import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity, Image } from "react-native";
 import styles from "./nav.style";
-
+import { icons } from "../../constants";
 const Nav = () => {
   return (
     <View style={styles.container}>
       <TouchableOpacity style={styles.navButton}>
-        <Text style={styles.navButtonText}>Button 1</Text>
+        <Image
+          style={styles.iconImg}
+          resizeMode="cover"
+          source={icons.transparency}
+        />
       </TouchableOpacity>
       <TouchableOpacity style={styles.navButton}>
-        <Text style={styles.navButtonText}>Button 2</Text>
+        <Text style={styles.navButtonText}>Match</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.navButton}>
-        <Text style={styles.navButtonText}>Button 3</Text>
+        <Text style={styles.navButtonText}>Boo's</Text>
       </TouchableOpacity>
     </View>
   );
--- app/components/Nav/nav.style.js
+++ app/components/Nav/nav.style.js
@@ -13,18 +13,26 @@ const styles = StyleSheet.create({
   },
   navButton: {
     flex: 1, // Distribute the available space equally among buttons
-    marginHorizontal: SIZES.small,
-    paddingVertical: SIZES.small,
-    paddingHorizontal: SIZES.medium,
-    backgroundColor: COLORS.primary,
-    borderRadius: SIZES.small,
+    // marginHorizontal: SIZES.small,
+    // paddingVertical: SIZES.small,
+    // paddingHorizontal: SIZES.medium,
+    backgroundColor: COLORS.white,
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: COLORS.white,
     justifyContent: "center",
     alignItems: "center",
+    color: COLORS.white,
+    height: 50,
   },
   navButtonText: {
-    color: COLORS.white,
-    fontFamily: FONT.bold, // Set your desired font family
-    fontSize: SIZES.body3,
+    color: COLORS.secondary,
+    fontFamily: FONT.regular, // Set your desired font family
+    fontSize: SIZES.small,
+  },
+  iconImg: {
+    width: 20,
+    height: 20,
   },
 });
 
