08e0a470d1a5a0339e5f6ec6121e063ab50f9680
Sleightttt <brennancota@yahoo.com>
2023-08-10 21:27:27 -0700

    added more quips

--- app/constants/headertags.js
+++ app/constants/headertags.js
@@ -1,1 +1,12 @@
-const headerTags = ["Find your boo"];
+const headerTags = [
+  "Swipe right to find your spark!",
+  "Connecting hearts, one swipe at a time.",
+  "Your journey to love starts here.",
+  "Flirting with destiny, swiping with purpose.",
+  "Unleash your charm and swipe away!",
+  "Because finding love should be an adventure.",
+  "Discover meaningful connections through swipes.",
+  "Matching souls, one swipe closer.",
+  "Bringing love and technology together.",
+  "Swipe, match, and let the magic begin.",
+];
