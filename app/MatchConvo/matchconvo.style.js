877d2ed521de441118d57273cb004eb63363aced
Sleightttt <brennancota@yahoo.com>
2023-08-18 14:09:51 -0700

    working on ui for matches page

---
+++ app/MatchConvo/matchconvo.style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+import { COLORS, FONT, SIZES } from ".././constants";
+
+const styles = StyleSheet.create({
+  matchBox: {
+    width: "100%",
+  },
+});
+
+export default styles;
